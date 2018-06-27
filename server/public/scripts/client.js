34db89fe64bc026600ce0ddf26f1f22feed9b67c
Danny Yang <dannyyaaj@gmail.com>
2018-06-27 17:07:12 -0500

    now appends quotes to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,30 @@ $(document).ready(docReady);
 
 function docReady() {
   console.log('jQuery loaded');
+  $('#generateBtn').on('click', handleBtnClick);
+}
+
+function handleBtnClick() {
+  console.log('Btn clicked');
+  $.ajax({
+    url: '/quotes',
+    type: 'GET'
+  }).done(function (response) {
+    console.log(response);
+    loopObj(response);
+  }).fail(function (errorResponse) {
+    console.log(errorResponse);
+    alert('Request Failed, error: ' + errorResponse.status);
+  })
+}
+
+function loopObj(object) {
+  for (let i = 0; i < object.length; i++) {
+    let ul = $('<ul class = "newUl"></ul>')
+    $('#listSection ul').append(`<li class="author">${object[i].author}</li>`);
+
+
+    $('.author').append(`<li>${object[i].text}</li>`);
+    // $('.newUl').
+  }
 }
