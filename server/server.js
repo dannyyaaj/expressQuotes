9cfcbf81e67b6c7455733df7bd76164cd84ffb43
Danny Yang <dannyyaaj@gmail.com>
2018-06-28 13:22:02 -0500

    sourced in body-parser

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 // Require express - gives us back a function
 const express = require('express');
 
+// get body-parser
+const bodyParser = require('body-parser');
+
 // Create an instance of express by calling the function
 // gives us back an object
 const app = express();
 const port = process.env.PORT || 5000; // Used to direct traffic
+
+// without this line, there is no data to be found on req.body
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Serves up static files
 app.use(express.static('server/public'));
@@ -26,6 +34,15 @@ app.get('/quotes', function (req, res) {
   let responseString = 'hello';
   res.send(quotes);
 });
+
+// post request
+app.post('/quotes', function (req, res) {
+  console.log('here is request.body', req.body);
+  // push to quotes array
+  quotes.push(req.body);
+  //send response
+  response.sendStatus(201);
+})
 
 // Start up the server
 app.listen(port, function () {
